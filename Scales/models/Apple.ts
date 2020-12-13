5544629f0415be1343cf92bbd6b95ae484640c3c
paulalexeev10@gmail.com <paulalexeev10@gmail.com>
2020-12-13 12:30:25 +0300

    *fixed

--- Scales/models/Apple.ts
+++ Scales/models/Apple.ts
@@ -1,7 +1,7 @@
 import Product from "./Product";
 
-export default class Apple extends Product{
-    constructor(kind: string = 'Apple',scale: number) {
+export default class Apple extends Product {
+    constructor(kind: string = 'Apple', scale: number) {
         super(kind, scale);
-    }      
+    }
 }
--- Scales/models/Tomato.ts
+++ Scales/models/Tomato.ts
@@ -1,7 +1,7 @@
 import Product from "./Product";
 
-export default class Tomato extends Product{
-    constructor(kind: string = 'Tomato',scale: number) {
+export default class Tomato extends Product {
+    constructor(kind: string = 'Tomato', scale: number) {
         super(kind, scale);
-    }    
+    }
 }
