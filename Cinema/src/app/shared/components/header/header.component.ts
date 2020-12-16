ad935ae715631d10fbc4b82484f63705c505b2f6
paulalexeev10@gmail.com <paulalexeev10@gmail.com>
2020-12-16 03:13:22 +0300

    +finished Cinema project

---
+++ Cinema/src/app/shared/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { TicketsService } from 'src/app/tickets.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private ticketsService: TicketsService) { }
+
+  ngOnInit(): void {
+  }
+
+  getCountFreeSeats(): number {
+    return this.ticketsService.getCountFreeSeats();
+  }
+
+  getCountNotFreeSeats(): number {
+    return this.ticketsService.getCountNotFreeSeats();
+  }
+}
---
+++ Cinema/src/app/shared/interfaces/ILowSeat.ts
@@ -0,0 +1,4 @@
+export  interface ILowSeat{
+    row: number;
+    numberSeat: number;
+}
